1ba2635e245f5a8f72d96b563d0dcf34f19de5ce
delucae <iut@>
2023-11-07 13:26:46 +0100

    ajout de la methode remplacer
    création de la classe CommandeRemplacer (vide)
    
    implementation de executer de CommandeRemplacer
    
    creation de la commande remplacer
    
    fix commande remplacer : ne print pas et ne prend pas en charge les string vide

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,25 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 3) {
+            System.err.println("Format attendu : remplacer;depart;fin;chaine");
+            return;
+        }
+        int depart = Integer.parseInt(parameters[1]);
+        int fin = Integer.parseInt(parameters[2]);
+        String texte = (parameters.length == 3) ? "" : parameters[3];
+        document.remplacer(depart,fin,texte);
+        super.executer();
+    }
+
+}
