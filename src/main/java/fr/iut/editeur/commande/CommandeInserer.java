e2af275aaa605858d4257e807eaac34aa3fc378c
Goetz_Hugo <hugo.goetz@etu.umontpellier.fr>
2023-11-09 14:55:50 +0100

    Inserer

---
+++ src/main/java/fr/iut/editeur/commande/CommandeInserer.java
@@ -0,0 +1,21 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeInserer extends CommandeDocument{
+
+    public CommandeInserer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    public void executer(){
+        if(parameters.length < 3){
+            System.err.println("Format attendu : inserer;position;chaine");
+            return;
+        }
+        int position = Integer.parseInt(parameters[1]);
+        String texte = (parameters.length == 2) ? "" : parameters[2];
+        document.inserer(position, texte);
+        super.executer();
+    }
+}
